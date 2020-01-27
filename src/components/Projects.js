0d0607ca94592c174b0675de3ee1404dfe08f35e
Jamshed Habibi <jamshed.habibi100@gmail.com>
2020-01-27 11:37:35 +0000

    Update to project portfolio

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,41 +1,51 @@
-import React, { Component } from 'react'
-import './Projects.css'
+import React, {Component} from 'react';
+import './Projects.css';
 
 export default class Projects extends Component {
-    render() {
-        const {img, name, id, link, github, react, bootstrap, js, css, contextApi} = this.props.project;
-        return (
-            <div className='col-8 col-md-6 col-lg-4 mx-auto p-3'>
-                    <div className='card'>
-                        <div className='portfolio-img img-container'>
-                            <img 
-                            className='img-thumbnail' 
-                            src={img} alt='portfolio project'/>
-                        </div>
+	render() {
+		const {
+			img,
+			name,
+			googleFirebase,
+			link,
+			github,
+			react,
+			bootstrap,
+			js,
+			css,
+			contextApi
+		} = this.props.project;
+		return (
+			<div className="col-8 col-md-6 col-lg-4 mx-auto p-3">
+				<div className="card">
+					<div className="portfolio-img img-container">
+						<img className="img-thumbnail" src={img} alt="portfolio project" />
+					</div>
 
-                        <div className='hover-details'>
-                            <div className='project-title text-center'>
-                                <p>{name}</p>
-                            </div>
+					<div className="hover-details">
+						<div className="project-title text-center">
+							<p>{name}</p>
+						</div>
 
-                            <div className='projectURL'>
-                                <a href={link} target='_blank'>
-                                <i className="px-2 fas fa-search"></i>
-                                </a>
-                                <a href={github} target='_blank'>
-                                    <i className="px-2 fab fa-github"></i>
-                                </a>
-                            </div>
+						<div className="projectURL">
+							<a href={link} target="_blank">
+								<i className="px-2 fas fa-search"></i>
+							</a>
+							<a href={github} target="_blank">
+								<i className="px-2 fab fa-github"></i>
+							</a>
+						</div>
 
-                
-                            <p className='f1'>{react} {js}</p>
-                            <p className='f2'>{contextApi}</p>
-                            <p className='f3'>{css}</p> 
-                            <p className='f4'>{bootstrap}</p>    
-                        </div>
-                    </div>
-                    
-            </div>
-        )
-    }
+						<p className="f5">{googleFirebase}</p>
+						<p className="f1">
+							{react} {js}
+						</p>
+						<p className="f2">{contextApi}</p>
+						<p className="f3">{css}</p>
+						<p className="f4">{bootstrap}</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
--- src/data.js
+++ src/data.js
@@ -1,83 +1,88 @@
 export const projectData = [
-    {
-        id: 1,
-        name: 'Real Estate Application',
-        link: 'https://jamsheds-real-estate.netlify.com',
-        img:  "https://michele.blog/wp-content/uploads/2017/02/handing-over-house-key.jpg",
-        github: "https://github.com/JamshedHabibi/real-estate",
-        react: "React",
-        contextApi: 'Context API',
-        css: 'Styled-Components',
-        bootstrap: 'Bootstrap',
-    },
-    {
-        id: 2,
-        name: 'E-Commerce Bike Shop',
-        link: 'https://jamsheds-ebike-shop.netlify.com',
-        img:  "https://img.archiexpo.com/images_ae/photo-g/153471-12012713.jpg",
-        github: "https://github.com/JamshedHabibi/react-bike-store",
-        react: "React",
-        contextApi: 'Context API',
-        css: 'Styled-Components',
-        bootstrap: 'Bootstrap',
-    },
-    {
-        id: 3,
-        name: 'Registration Form',
-        link: 'https://jamsheds-registration-form.netlify.com',
-        img: 'https://www.thebalancesmb.com/thmb/tBT83K1QjTZQech2ETDJ0-dLqSU=/3840x3840/smart/filters:no_upscale()/GettyImages-608156065-572f6d393df78c038e42aaed.jpg',
-        github: 'https://github.com/JamshedHabibi/registration-form',
-        js: "JavaScript",
-        css: 'CSS'
-
-    },
-    {
-        id: 4,
-        name: 'Guess The Number',
-        link: 'https://jamsheds-number-guessing-game.netlify.com',
-        img: 'https://static.wixstatic.com/media/0b5415db01d9447f88f43a93d9d23261.jpg/v1/fill/w_1024,h_1024,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01/0b5415db01d9447f88f43a93d9d23261.jpg',
-        github: 'https://github.com/JamshedHabibi/number-guessing-game',
-        js: 'JavaScript',
-        css: 'CSS'
-    }
-]
+	{
+		id: 1,
+		name: 'Melbourne Tours',
+		link: 'https://melbourne-tours.netlify.com',
+		img:
+			'https://www.tripsavvy.com/thmb/jO1lBr8jp3HVVuAyGIf3ChGhsts=/1428x1428/smart/filters:no_upscale()/cityscape-of-melbourne-city-of-australia-during-the-sunset--873409300-8db98cef20b44200abfb339188d801f7.jpg',
+		github: 'https://github.com/JamshedHabibi/melbourne-tours',
+		react: 'React',
+		contextApi: 'Context API',
+		googleFirebase: 'Google Firebase',
+		css: 'CSS',
+		bootstrap: 'React Semantic UI'
+	},
+	{
+		id: 1,
+		name: 'Real Estate Application',
+		link: 'https://jamsheds-real-estate.netlify.com',
+		img:
+			'https://michele.blog/wp-content/uploads/2017/02/handing-over-house-key.jpg',
+		github: 'https://github.com/JamshedHabibi/real-estate',
+		react: 'React',
+		contextApi: 'Context API',
+		css: 'Styled-Components',
+		bootstrap: 'Bootstrap'
+	},
+	{
+		id: 2,
+		name: 'E-Commerce Bike Shop',
+		link: 'https://jamsheds-ebike-shop.netlify.com',
+		img: 'https://img.archiexpo.com/images_ae/photo-g/153471-12012713.jpg',
+		github: 'https://github.com/JamshedHabibi/react-bike-store',
+		react: 'React',
+		contextApi: 'Context API',
+		css: 'Styled-Components',
+		bootstrap: 'Bootstrap'
+	},
+	{
+		id: 3,
+		name: 'Registration Form',
+		link: 'https://jamsheds-registration-form.netlify.com',
+		img:
+			'https://www.thebalancesmb.com/thmb/tBT83K1QjTZQech2ETDJ0-dLqSU=/3840x3840/smart/filters:no_upscale()/GettyImages-608156065-572f6d393df78c038e42aaed.jpg',
+		github: 'https://github.com/JamshedHabibi/registration-form',
+		js: 'JavaScript',
+		css: 'CSS'
+	}
+];
 
 export const skills = [
-    {
-        id: 1,
-        name: 'JavaScript',
-        img: 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/205/square_280/javascriptlang.png'
-    },
-    {
-        id: 2,
-        name: 'HTML',
-        img: 'https://cdn0.iconfinder.com/data/icons/HTML5/512/HTML_Logo.png'
-    },
-    {
-        id: 3,
-        name: 'CSS',
-        img: 'https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_960_720.png'
-    },
-    {
-        id: 4,
-        name: 'React',
-        img: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png'
-    },
-    {
-        id: 5,
-        name: 'Bootstrap',
-        img: 'https://suavesol.net/wp-content/uploads/2019/06/bootstrap.png'
-    },
-    {
-        id: 6,
-        name: 'JQuery',
-        img: 'http://pluspng.com/img-png/jquery-logo-png--800.gif'
-    },
-    {
-        id: 7,
-        name: 'NodeJS',
-        img: 'https://www.cnjobs.dk/drupal/sites/default/files/2019-01/nodejs-01.png'
-    },
-
-    
-]
+	{
+		id: 1,
+		name: 'JavaScript',
+		img:
+			'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/205/square_280/javascriptlang.png'
+	},
+	{
+		id: 2,
+		name: 'HTML',
+		img: 'https://cdn0.iconfinder.com/data/icons/HTML5/512/HTML_Logo.png'
+	},
+	{
+		id: 3,
+		name: 'CSS',
+		img: 'https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_960_720.png'
+	},
+	{
+		id: 4,
+		name: 'React',
+		img:
+			'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png'
+	},
+	{
+		id: 5,
+		name: 'Bootstrap',
+		img: 'https://suavesol.net/wp-content/uploads/2019/06/bootstrap.png'
+	},
+	{
+		id: 6,
+		name: 'JQuery',
+		img: 'http://pluspng.com/img-png/jquery-logo-png--800.gif'
+	},
+	{
+		id: 7,
+		name: 'NodeJS',
+		img: 'https://www.cnjobs.dk/drupal/sites/default/files/2019-01/nodejs-01.png'
+	}
+];
