f88d73456ce15cce2c1e2fe03f9bdc8caabd906e
Jamshed Habibi <jamshed.habibi@davidlloyd.co.uk>
2021-10-28 13:28:12 +0100

    Updated image for stopwatch project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,41 +1,43 @@
-import React, { Component } from 'react'
-import './Portfolio.css'
-import { ProjectConsumer } from '../Context'
-import Projects from './Projects'
-
+import React, { Component } from 'react';
+import './Portfolio.css';
+import { ProjectConsumer } from '../Context';
+import Projects from './Projects';
 
 export default class Portfolio extends Component {
-    render() {
-        return (
-            <div className='Portfolio'>
-                <div className='portfolioTitle pt-2'>
-                        <h1>My Portfolio</h1>
-                </div>
+  render() {
+    return (
+      <div className="Portfolio">
+        <div className="portfolioTitle pt-2">
+          <h1>My Portfolio</h1>
+        </div>
 
-                <div className='portfolio-projects container pt-md-5 mt-5 pb-4'>
-                    <div className='row py-5 mb-4'>
-                        <ProjectConsumer>
-                            {context => {
-                                return context.projects.map(project => {
-                                    return <Projects key={project.id} project={project}/>
-                                })
-                            }}
-                        </ProjectConsumer>
-                        
-                    </div>
-                </div>
+        <div className="portfolio-projects container pt-md-5 mt-5 pb-4">
+          <div className="row py-5 mb-4">
+            <ProjectConsumer>
+              {context => {
+                return context.projects.map(project => {
+                  return <Projects key={project.id} project={project} />;
+                });
+              }}
+            </ProjectConsumer>
+          </div>
+        </div>
 
-                <ProjectConsumer>
-                {value => {
-                    return <div className='arrow-container3'>
-                                <i 
-                                className="arrowBtn3 fas fa-chevron-down"
-                                onClick={() => {value.arrowScroll('.section4', 45)}}></i>
-                                
-                            </div>
-                    }}
-                </ProjectConsumer>
-            </div>
-        )
-    }
+        <ProjectConsumer>
+          {value => {
+            return (
+              <div className="arrow-container3">
+                <i
+                  className="arrowBtn3 fas fa-chevron-down"
+                  onClick={() => {
+                    value.arrowScroll('.section4', 45);
+                  }}
+                ></i>
+              </div>
+            );
+          }}
+        </ProjectConsumer>
+      </div>
+    );
+  }
 }
--- src/data.js
+++ src/data.js
@@ -1,100 +1,94 @@
 export const projectData = [
-	{
-		id: 0,
-		name: 'Melbourne Tours',
-		link: 'https://melbourne-tours.netlify.com',
-		img:
-			'https://www.tripsavvy.com/thmb/jO1lBr8jp3HVVuAyGIf3ChGhsts=/1428x1428/smart/filters:no_upscale()/cityscape-of-melbourne-city-of-australia-during-the-sunset--873409300-8db98cef20b44200abfb339188d801f7.jpg',
-		github: 'https://github.com/JamshedHabibi/melbourne-tours',
-		react: 'React',
-		contextApi: 'Context API',
-		googleFirebase: 'Google Firebase',
-		css: 'CSS',
-		bootstrap: 'React Semantic UI'
-	},
-	{
-		id: 1,
-		name: 'Real Estate Application',
-		link: 'https://jamsheds-real-estate.netlify.com',
-		img:
-			'https://michele.blog/wp-content/uploads/2017/02/handing-over-house-key.jpg',
-		github: 'https://github.com/JamshedHabibi/real-estate',
-		react: 'React',
-		contextApi: 'Context API',
-		css: 'Styled-Components',
-		bootstrap: 'Bootstrap'
-	},
-	{
-		id: 2,
-		name: 'E-Commerce Bike Shop',
-		link: 'https://jamsheds-ebike-shop.netlify.com',
-		img: 'https://img.archiexpo.com/images_ae/photo-g/153471-12012713.jpg',
-		github: 'https://github.com/JamshedHabibi/react-bike-store',
-		react: 'React',
-		contextApi: 'Context API',
-		css: 'Styled-Components',
-		bootstrap: 'Bootstrap'
-	},
-	{
-		id: 3,
-		name: 'Registration Form',
-		link: 'https://jamsheds-registration-form.netlify.com',
-		img:
-			'https://www.thebalancesmb.com/thmb/tBT83K1QjTZQech2ETDJ0-dLqSU=/3840x3840/smart/filters:no_upscale()/GettyImages-608156065-572f6d393df78c038e42aaed.jpg',
-		github: 'https://github.com/JamshedHabibi/registration-form',
-		js: 'JavaScript',
-		css: 'CSS'
-	},
-	{
-		id: 4,
-		name: 'Cool Stopwatch',
-		link: 'https://jamsheds-stopwatch.netlify.com',
-		img:
-			'https://images.wallpaperscraft.com/image/compass_hand_journey_136931_3415x3415.jpg',
-		github: 'https://github.com/JamshedHabibi/stopwatch',
-		react: 'React',
-		contextApi: 'Context API',
-		css: 'CSS',
-		bootstrap: 'Bootstrap'
-	}
+  {
+    id: 0,
+    name: 'Melbourne Tours',
+    link: 'https://melbourne-tours.netlify.com',
+    img: 'https://www.tripsavvy.com/thmb/jO1lBr8jp3HVVuAyGIf3ChGhsts=/1428x1428/smart/filters:no_upscale()/cityscape-of-melbourne-city-of-australia-during-the-sunset--873409300-8db98cef20b44200abfb339188d801f7.jpg',
+    github: 'https://github.com/JamshedHabibi/melbourne-tours',
+    react: 'React',
+    contextApi: 'Context API',
+    googleFirebase: 'Google Firebase',
+    css: 'CSS',
+    bootstrap: 'React Semantic UI',
+  },
+  {
+    id: 1,
+    name: 'Real Estate Application',
+    link: 'https://jamsheds-real-estate.netlify.com',
+    img: 'https://michele.blog/wp-content/uploads/2017/02/handing-over-house-key.jpg',
+    github: 'https://github.com/JamshedHabibi/real-estate',
+    react: 'React',
+    contextApi: 'Context API',
+    css: 'Styled-Components',
+    bootstrap: 'Bootstrap',
+  },
+  {
+    id: 2,
+    name: 'E-Commerce Bike Shop',
+    link: 'https://jamsheds-ebike-shop.netlify.com',
+    img: 'https://img.archiexpo.com/images_ae/photo-g/153471-12012713.jpg',
+    github: 'https://github.com/JamshedHabibi/react-bike-store',
+    react: 'React',
+    contextApi: 'Context API',
+    css: 'Styled-Components',
+    bootstrap: 'Bootstrap',
+  },
+  {
+    id: 3,
+    name: 'Registration Form',
+    link: 'https://jamsheds-registration-form.netlify.com',
+    img: 'https://www.thebalancesmb.com/thmb/tBT83K1QjTZQech2ETDJ0-dLqSU=/3840x3840/smart/filters:no_upscale()/GettyImages-608156065-572f6d393df78c038e42aaed.jpg',
+    github: 'https://github.com/JamshedHabibi/registration-form',
+    js: 'JavaScript',
+    css: 'CSS',
+  },
+  {
+    id: 4,
+    name: 'Cool Stopwatch',
+    link: 'https://jamsheds-stopwatch.netlify.com',
+    github: 'https://github.com/JamshedHabibi/stopwatch',
+    img: 'https://s.abcnews.com/images/US/time-change-02-ap-jc-180307_1x1_608.jpg',
+    react: 'React',
+    contextApi: 'Context API',
+    css: 'CSS',
+    bootstrap: 'Bootstrap',
+  },
 ];
 
 export const skills = [
-	{
-		id: 1,
-		name: 'JavaScript',
-		img:
-			'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/205/square_280/javascriptlang.png'
-	},
-	{
-		id: 2,
-		name: 'HTML',
-		img: 'https://cdn0.iconfinder.com/data/icons/HTML5/512/HTML_Logo.png'
-	},
-	{
-		id: 3,
-		name: 'CSS',
-		img: 'https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_960_720.png'
-	},
-	{
-		id: 4,
-		name: 'React',
-		img:
-			'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png'
-	},
-	{
-		id: 5,
-		name: 'Bootstrap',
-		img: 'https://suavesol.net/wp-content/uploads/2019/06/bootstrap.png'
-	},
-	{
-		id: 6,
-		name: 'JQuery',
-		img: 'http://pluspng.com/img-png/jquery-logo-png--800.gif'
-	},
-	{
-		id: 7,
-		name: 'NodeJS',
-		img: 'https://www.cnjobs.dk/drupal/sites/default/files/2019-01/nodejs-01.png'
-	}
+  {
+    id: 1,
+    name: 'JavaScript',
+    img: 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/205/square_280/javascriptlang.png',
+  },
+  {
+    id: 2,
+    name: 'HTML',
+    img: 'https://cdn0.iconfinder.com/data/icons/HTML5/512/HTML_Logo.png',
+  },
+  {
+    id: 3,
+    name: 'CSS',
+    img: 'https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_960_720.png',
+  },
+  {
+    id: 4,
+    name: 'React',
+    img: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png',
+  },
+  {
+    id: 5,
+    name: 'Bootstrap',
+    img: 'https://suavesol.net/wp-content/uploads/2019/06/bootstrap.png',
+  },
+  {
+    id: 6,
+    name: 'JQuery',
+    img: 'http://pluspng.com/img-png/jquery-logo-png--800.gif',
+  },
+  {
+    id: 7,
+    name: 'NodeJS',
+    img: 'https://www.cnjobs.dk/drupal/sites/default/files/2019-01/nodejs-01.png',
+  },
 ];
